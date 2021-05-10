db485e8b4ea4fa46f3bc9cbb3304209f8c341fa8
mnechip <64624310+mnechip@users.noreply.github.com>
2021-05-10 15:58:24 +1000

    v0.2.0
    More functional, works with all numbers however is quite inefficient and could be inaccurate. May need to create a larger list of base primes to avoid establishing primes for every number.

--- prime multiple finder.py
+++ prime multiple finder.py
@@ -1,36 +1,45 @@
 import math
 print("Number to find prime of: ")
 num = int(input())
-rootNum = math.sqrt(num)
+rootNum = 4*(math.sqrt(num))
 roundedNum = int(rootNum) #sets up the variables, converts the float root to an int for processing
 
-primes = [2,3,5,7] #sets a list of 'base primes'
+primes = [] #sets a list of 'base primes'
 factors = [] #empty list of factors that will be filled in later
-i = 8 #sets i to 8 as the last prime was 7
+ #sets i to 8 as the last prime was 7
 
 #if the square root of the original number is even, make it odd by subtracting one
-if roundedNum%2 != 0:
-    oddRounded = roundedNum - 1
-else:
-    oddRounded = roundedNum
+lastDigits = []
+oddNumbers = []
+def primeNumCheck(x):
+    if x >= 2:
+        for otherNum in range (2, x):
+            if not ( x % otherNum):
+                return False
+    else:
+        return False
+    return True
+for y in range (2, roundedNum):
+    if y%2 !=0 and y%3 !=0 and y%5 !=0 and y%7 !=0:
+        oddNumbers.append(y)
 
-#for every integer in the range of 8 to SQRT(Original), check if it is a prime by testing divisibility by base primes
-for i in range (i, oddRounded):
-    if i%2 !=0:
-        if i%3 !=0:
-            if i%5 !=0:
-                if i%7 !=0:
-                        primes.append(i) #if it is a prime, append it to the list of primes that range between 2 - SQRT
+for y in oddNumbers:
+    if primeNumCheck(y):
+        primes.append(y)
 
 #for every number in the primes list, check if its a factor of the original number
-for i in primes:
-    if num%i == 0:
-        factors.append(i)
-        print("The prime factors of " + str(num) + " are: ")
-        print(factors)
-        print("The highest prime of " + str(num) + " is: ")
-        print(factors[-1])
-    elif i == num:
-        print(str(num) + " is a prime.")
-        
+if num in {2, 3, 5, 7}:
+    print(str(num) + " is a prime number.")
+elif num in {0, 1}:
+    print("Number is 0 or 1, undefined.")
+elif primeNumCheck(num) == True:
+    print(str(num) + " is a prime.")
+else:
+    for x in primes:
+        if num%x == 0:
+            factors.append(x)
+    print("The prime factors of " + str(num) + " are: ")
+    print(factors)
+    print("The highest prime of " + str(num) + " is: ")
+    print(factors[-1:])
     
